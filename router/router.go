4a486a750d0fbe0632a8532e687de7520fc26c91
DamiaoCanndido <tower_gamer@hotmail.com>
2023-11-05 19:11:32 -0300

    split routes

--- router/router.go
+++ router/router.go
@@ -1,17 +1,11 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(r)
 	r.Run(":3333")
 }
