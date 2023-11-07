816b0dafc360861e49b2de7478eaae593827d20f
DamiaoCanndido <tower_gamer@hotmail.com>
2023-11-07 18:45:22 -0300

    show openings

--- handler/showOpenings.go
+++ handler/showOpenings.go
@@ -3,11 +3,15 @@ package handler
 import (
 	"net/http"
 
+	"github.com/DamiaoCanndido/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error show openings")
+		return
+	}
+	sendSuccess(ctx, "list openings", openings)
 }
